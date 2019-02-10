848cd24116291237e433bf49d14e944b3642f5e0
eatthatpie <tomek.pugn@gmail.com>
2019-02-10 14:42:58 +0100

    update main entry file

--- src/Main.ts
+++ src/Main.ts
@@ -1,9 +1,20 @@
 import GameLauncher from '@/game/GameLauncher';
 
+// AppContext.extendConfig({
+//     input: {
+//         keymap: {
+//             'arrow.down': function() {...},
+//             'arrow.up': function() {...}
+//         }
+//     }
+// });
+
+// AppContext.getConfig('input.keymap')
+
 (function () {
     GameLauncher.launch({
         DOMElementSelector: '#game',
-        width: 500,
+        width: 800,
         height: 600
     });
 })();
