1a154957f2cd53e70ce974fe3171ea9d152c2101
eatthatpie <tomek.pugn@gmail.com>
2019-06-16 21:08:18 +0200

    small tweak

--- src/gameplay/grid/CollapsingGridBlock.spec.ts
+++ src/gameplay/grid/CollapsingGridBlock.spec.ts
@@ -1,5 +1,5 @@
 import CollapsingGridBlock from './CollapsingGridBlock';
 
 describe(`Collapsing grid block`, () => {
-    // test(`cells with the same type in the same row can be removed cause the other taken cells collapsed`, () => {});
+    // test(`cells with the same type in the same row can be removed and cause the other taken cells collapsed`, () => {});
 });
